ef6dc5166c86227e5136bc93dbef2d8482f3582b
kewablay <kojokewajunior123@gmail.com>
2024-10-01 15:43:48 +0000

    testing logout

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BlogPostService } from '../../services/blog-post/blog-post.service';
+import { AuthService } from '../../services/auth-service/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -12,10 +13,17 @@ import { BlogPostService } from '../../services/blog-post/blog-post.service';
 export class HomeComponent {
   posts$!: Observable<unknown[]>;
 
-  constructor(private blogPostsService: BlogPostService) {}
+  constructor(
+    private blogPostsService: BlogPostService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
     this.posts$ = this.blogPostsService.getBlogPosts();
     this.posts$.subscribe((posts) => console.log('posts:', posts));
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 }
