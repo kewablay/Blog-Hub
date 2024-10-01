b148e07defe9c8106424781afafbc53f90e8ebba
kewablay <kojokewajunior123@gmail.com>
2024-10-01 12:45:34 +0000

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./features/signup/signup.component').then(
+        (m) => m.SignupComponent
+      ),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'post/:id',
+    loadComponent: () =>
+      import('./features/post-detail/post-detail.component').then(
+        (m) => m.PostDetailComponent
+      ),
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
