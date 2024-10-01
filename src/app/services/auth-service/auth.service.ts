9b832cee75bd885b5d8698a6f3e2c5dcada0b82b
kewablay <kojokewajunior123@gmail.com>
2024-10-01 15:43:36 +0000

    added logout

--- src/app/services/auth-service/auth.service.ts
+++ src/app/services/auth-service/auth.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@angular/fire/auth';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from '@angular/fire/auth';
 import { addDoc, collection, Firestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(private firestore: Firestore, private auth: Auth) {}
+  constructor(
+    private firestore: Firestore,
+    private auth: Auth,
+    private router: Router
+  ) {}
 
   login(email: string, password: string) {
     return signInWithEmailAndPassword(this.auth, email, password);
@@ -22,5 +30,15 @@ export class AuthService {
         return addDoc(usersCollection, { id: userId, username: username });
       }
     );
+  }
+
+  logout() {
+    // this.router.navigate(['/login']);
+    return this.auth
+      .signOut()
+      .then(() => {
+        this.router.navigate(['/login']);
+      })
+      .catch((err) => console.log('error occured while logging out', err));
   }
 }
