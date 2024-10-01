64684732de4cdbbdfdd0099b7e4fc45eda46d47b
kewablay <kojokewajunior123@gmail.com>
2024-10-01 11:20:27 +0000

    added first blog post service in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BlogPostService } from './services/blog-post.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
 export class AppComponent {
-  title = 'blog-hub';
+  posts$!: Observable<unknown[]>;
+
+  constructor(private blogPostsService: BlogPostService) {}
+
+  ngOnInit() {
+    this.posts$ = this.blogPostsService.getBlogPosts();
+    this.posts$.subscribe((posts) => console.log('posts:', posts));
+  }
 }
