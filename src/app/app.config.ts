2e83746b1a8fca3a3690fb9f7101cc5565eae2a7
kewablay <kojokewajunior123@gmail.com>
2024-10-01 10:19:24 +0000

    reset code to initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,39 @@
-import { ApplicationConfig } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  isDevMode,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
+import { environment } from '../environments/environment.development';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import {
+  provideAnalytics,
+  getAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService,
+  ],
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDJyxKAPDfEoicZwoe6erp9qWrpAkJkYsE',
+    authDomain: 'blog-hub-c67cf.firebaseapp.com',
+    projectId: 'blog-hub-c67cf',
+    storageBucket: 'blog-hub-c67cf.appspot.com',
+    messagingSenderId: '85948031090',
+    appId: '1:85948031090:web:f853a3a3bea85278df62eb',
+    measurementId: 'G-0LFH0E06G1',
+  },
+};
