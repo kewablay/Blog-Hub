67f86f2d2bdd50f5896248ada221ffef62ec6677
kewablay <kojokewajunior123@gmail.com>
2024-10-01 15:44:53 +0000

    added route guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -14,6 +15,7 @@ export const routes: Routes = [
       ),
   },
   {
+    canActivate: [authGuard],
     path: '',
     loadComponent: () =>
       import('./features/home/home.component').then((m) => m.HomeComponent),
