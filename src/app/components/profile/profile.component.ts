910ead00613e3382949fd206415f620360d68740
kewablay <kojokewajunior123@gmail.com>
2024-10-03 15:32:17 +0000

    added profile component

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,14 +1,26 @@
 import { Component, Input } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { from, Observable } from 'rxjs';
+import { DocumentData } from '@angular/fire/firestore';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.sass',
 })
 export class ProfileComponent {
   @Input() authorId!: string;
+  user$!: Observable<DocumentData | undefined>;
 
-  
+  constructor(private userService: UserService) {}
+
+
+  ngOnInit() {
+    console.log('authorId:', this.authorId);
+    this.user$ = from(this.userService.getUserById(this.authorId));
+    this.user$.subscribe((user) => console.log('user:', user));
+  }
 }
