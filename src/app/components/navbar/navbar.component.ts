693c30b26be549b097dec604abdf19fcdac2539d
kewablay <kojokewajunior123@gmail.com>
2024-10-03 09:52:13 +0000

    added navbar content with responsiveness

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ProfileComponent } from "../profile/profile.component";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [ProfileComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.sass'
 })
