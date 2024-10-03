76d19a7c8a8f2e864af3ed866d3affc66d1a6e00
kewablay <kojokewajunior123@gmail.com>
2024-10-03 15:31:50 +0000

    updated navbar with router link

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { ProfileComponent } from "../profile/profile.component";
+import { ProfileComponent } from '../profile/profile.component';
+import { AuthService } from '../../services/auth-service/auth.service';
+import { Auth, user, User } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import { DocumentData } from '@angular/fire/firestore';
+import { AsyncPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ProfileComponent],
+  imports: [ProfileComponent, AsyncPipe, RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.sass'
+  styleUrl: './navbar.component.sass',
 })
 export class NavbarComponent {
+  user$!: Observable<User | null>;
 
+  constructor(private auth: Auth) {}
+
+  ngOnInit() {
+    this.user$ = user(this.auth);
+  }
 }
