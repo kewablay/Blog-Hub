da3eb27d265a406b774cc3fee297f74383f57a09
kewablay <kojokewajunior123@gmail.com>
2024-10-03 15:32:37 +0000

    added blgo detail integration

--- src/app/features/blog-detail/blog-detail.component.ts
+++ src/app/features/blog-detail/blog-detail.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { ProfileComponent } from '../../components/profile/profile.component';
+import { BlogPostService } from '../../services/blog-post/blog-post.service';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, switchMap } from 'rxjs';
+import { DocumentData } from '@angular/fire/firestore';
+import { AsyncPipe } from '@angular/common';
+import { CommentsListComponent } from "../../components/comments-list/comments-list.component";
 
 @Component({
   selector: 'app-blog-detail',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, ProfileComponent, AsyncPipe, CommentsListComponent],
   templateUrl: './blog-detail.component.html',
-  styleUrl: './blog-detail.component.sass'
+  styleUrl: './blog-detail.component.sass',
 })
 export class BlogDetailComponent {
+  blogPost$: Observable<DocumentData | undefined>;
+  blogPostId!: string;
+  constructor(
+    private blogPostsService: BlogPostService,
+    private route: ActivatedRoute
+  ) {
+    this.blogPost$ = this.route.paramMap.pipe(
+      switchMap((params) => {
+        const blogId = params.get('id');
+        this.blogPostId = blogId as string;
+        return this.blogPostsService.getBlogById(blogId as string);
+      })
+    );
+  }
 
 }
