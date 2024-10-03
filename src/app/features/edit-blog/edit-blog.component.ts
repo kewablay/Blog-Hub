bf2e64415a51d6f1eda7e0ae382fdf6aa6397e69
kewablay <kojokewajunior123@gmail.com>
2024-10-03 16:46:35 +0000

    added edit blog page

---
+++ src/app/features/edit-blog/edit-blog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-blog',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-blog.component.html',
+  styleUrl: './edit-blog.component.sass'
+})
+export class EditBlogComponent {
+
+}
