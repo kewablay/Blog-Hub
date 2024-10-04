437388b74426e8dcf60e65f0f8a57d54cb93f0cf
kewablay <kojokewajunior123@gmail.com>
2024-10-04 08:47:08 +0000

    added proper date format for comment and blog post.

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { ProfileComponent } from '../profile/profile.component';
 import { DocumentData } from '@angular/fire/firestore';
 import { TruncatePipe } from '../../pipes/truncate.pipe';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [ProfileComponent, TruncatePipe],
+  imports: [ProfileComponent, TruncatePipe, DatePipe],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.sass',
 })
--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,16 +1,46 @@
-import { Component, Input } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
 import { DocumentData } from '@angular/fire/firestore';
-import { ProfileComponent } from "../profile/profile.component";
+import { ProfileComponent } from '../profile/profile.component';
+import { Auth, user } from '@angular/fire/auth';
+import { combineLatest } from 'rxjs';
+import { DatePipe } from '@angular/common';
+import { CommentsService } from '../../services/comments-service/comments.service';
+import { NOTYF } from '../../utils/notyf.token';
+import { Notyf } from 'notyf';
 
 @Component({
   selector: 'app-comment',
   standalone: true,
-  imports: [ProfileComponent],
+  imports: [ProfileComponent, DatePipe],
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.sass',
 })
 export class CommentComponent {
   @Input() comment!: DocumentData;
+  isCommentAuthor: boolean = false;
 
-  
+  constructor(
+    private auth: Auth,
+    private commentsService: CommentsService,
+    @Inject(NOTYF) private notyf: Notyf
+  ) {}
+
+  ngOnInit() {
+    user(this.auth).subscribe((user) => {
+      if (user) {
+        this.isCommentAuthor = user.uid === this.comment['authorId'];
+      }
+    });
+  }
+
+  onDelete() {
+    this.commentsService.deleteComment(this.comment['id']).subscribe({
+      next: () => {
+        this.notyf.success('Comment deleted successfully');
+      },
+      error: () => {
+        this.notyf.error('Failed to delete comment');
+      },
+    });
+  }
 }
