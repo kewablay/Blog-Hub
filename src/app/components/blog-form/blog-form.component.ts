8283aa77ac4fa3d84bc5084aa06e20a669490a43
kewablay <kojokewajunior123@gmail.com>
2024-10-04 14:29:05 +0000

    updated blog form

--- src/app/components/blog-form/blog-form.component.ts
+++ src/app/components/blog-form/blog-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -11,18 +11,23 @@ import { Notyf } from 'notyf';
 import { NOTYF } from '../../utils/notyf.token';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { EditorModule } from 'primeng/editor';
+import { DocumentData } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-blog-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, EditorModule],
   templateUrl: './blog-form.component.html',
   styleUrl: './blog-form.component.sass',
 })
 export class BlogFormComponent {
+  @Input() blog!: DocumentData;
+  @Input() blogPostId!: string;
   user$!: Observable<User | null>;
   userId: string | undefined;
-  createBlogLoading: boolean = false;
+  isLoading: boolean = false;
+  isEditMode: boolean = false;
 
   postForm = new FormGroup({
     title: new FormControl('', [Validators.required]),
@@ -44,31 +49,63 @@ export class BlogFormComponent {
         this.userId = user?.uid;
       }
     });
+
+    // SHOW BLOG DATA IF IT IS EDIT BLOG
+    if (this.blog) {
+      // this.isEditMode = true;
+      this.postForm.patchValue({
+        title: this.blog['title'],
+        content: this.blog['content'],
+      });
+    }
   }
 
   onSubmit() {
     if (this.postForm.valid) {
-      this.createBlogLoading = true;
-      this.blogPostService
-        .createBlogPost({
-          title: this.postForm.value.title,
-          content: this.postForm.value.content,
-          dateCreated: new Date(),
-          likes: 0,
-          dislikes: 0,
-          authorId: this.userId,
-        })
-        .then(() => {
-          this.createBlogLoading = false;
-          this.notyf.success('Post created successfully');
-          this.postForm.reset();
-          this.router.navigate(['/']);
-        })
-        .catch((err) => {
-          this.createBlogLoading = false;
-          this.notyf.error('Failed to create post');
-          console.error('Login failed', err.message);
-        });
+      // UPDATE BLOG POST
+      if (this.blog) {
+        this.isLoading = true;
+        this.blogPostService
+          .updateBlogPost(this.blogPostId, {
+            title: this.postForm.value.title,
+            content: this.postForm.value.content,
+          })
+          .subscribe({
+            next: () => {
+              this.isLoading = false;
+              this.notyf.success('Post updated successfully');
+              this.router.navigate(['/']);
+            },
+            error: (err) => {
+              this.isLoading = false;
+              this.notyf.error('Failed to update post');
+              console.error('Login failed', err.message);
+            },
+          });
+      } else {
+        // CREATE BLOG POST
+        this.isLoading = true;
+        this.blogPostService
+          .createBlogPost({
+            title: this.postForm.value.title,
+            content: this.postForm.value.content,
+            dateCreated: new Date(),
+            likes: 0,
+            dislikes: 0,
+            authorId: this.userId,
+          })
+          .then(() => {
+            this.isLoading = false;
+            this.notyf.success('Post created successfully');
+            this.postForm.reset();
+            this.router.navigate(['/']);
+          })
+          .catch((err) => {
+            this.isLoading = false;
+            this.notyf.error('Failed to create post');
+            console.error('Login failed', err.message);
+          });
+      }
     }
   }
 }
