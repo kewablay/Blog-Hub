d2aed06353f9e5f508d837f912550e3f212f2f65
kewablay <kojokewajunior123@gmail.com>
2024-10-05 22:46:23 +0000

    added google signup to auth

--- src/app/services/auth-service/auth.service.ts
+++ src/app/services/auth-service/auth.service.ts
@@ -2,8 +2,11 @@ import { Injectable } from '@angular/core';
 import {
   Auth,
   createUserWithEmailAndPassword,
+  GoogleAuthProvider,
   signInWithEmailAndPassword,
+  signInWithPopup,
   user,
+  UserCredential,
 } from '@angular/fire/auth';
 import { addDoc, collection, Firestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
@@ -43,6 +46,25 @@ export class AuthService {
       }
     );
   }
+
+  googleSignIn(): Promise<UserCredential> {
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(this.auth, provider).then((result) => {
+      const user = result.user;
+      const usersCollection = collection(this.firestore, 'users');
+      
+      // Check if user already exists, if not add to Firestore
+      return addDoc(usersCollection, {
+        uid: user.uid,
+        username: user.displayName,
+        email: user.email,
+        avatarUrl: user.photoURL,
+      }).then(() => result);
+    });
+  }
+
+
+  
 
   logout() {
     // this.router.navigate(['/login']);
