47f46f65ca0fb91c117a29d670fa201b6b1a00a5
kewablay <kojokewajunior123@gmail.com>
2024-10-06 00:58:21 +0000

    updated blog post service to show cached post when offline

--- src/app/services/blog-post/blog-post.service.ts
+++ src/app/services/blog-post/blog-post.service.ts
@@ -10,24 +10,65 @@ import {
   getDoc,
   updateDoc,
 } from '@angular/fire/firestore';
-import { from, map, Observable } from 'rxjs';
+import { catchError, from, map, Observable, of, tap } from 'rxjs';
+import { LocalStorageService } from '../localStorageService/local-storage.service';
+import { OnlineStatusService } from '../online-status/online-status.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BlogPostService {
-  constructor(private firestore: Firestore) {}
+  constructor(
+    private firestore: Firestore,
+    private localStorageService: LocalStorageService,
+    private onlineStatusService: OnlineStatusService
+  ) {}
 
   getBlogPosts(): Observable<DocumentData[]> {
-    const postCollection = collection(this.firestore, 'posts');
-    return collectionData(postCollection, { idField: 'id' });
+    if (this.onlineStatusService.isOnline()) {
+      const postCollection = collection(this.firestore, 'posts');
+      return collectionData(postCollection, { idField: 'id' }).pipe(
+        // here we cache blog posts to handle offline viewing
+        tap((posts) => {
+          this.localStorageService.setItem('cachedBlogPosts', posts);
+        }),
+        catchError(() => {
+          const cachedPosts =
+            this.localStorageService.getItem('cachedBlogPosts');
+          return cachedPosts ? of(cachedPosts) : of([]);
+        })
+      );
+    } else {
+      const cachedPosts = this.localStorageService.getItem('cachedBlogPosts');
+      return cachedPosts ? of(cachedPosts) : of([]);
+    }
   }
 
   getBlogById(id: string): Observable<DocumentData | undefined> {
-    const postDoc = doc(this.firestore, 'posts', id);
-    return from(getDoc(postDoc)).pipe(
-      map((docSnap) => (docSnap.exists() ? docSnap.data() : undefined))
-    );
+    // IF USER IS ONLINE GET BLOG POST FROM FIRESTORE
+    if (this.onlineStatusService.isOnline()) {
+      const postDoc = doc(this.firestore, 'posts', id);
+      return from(getDoc(postDoc)).pipe(
+        map((docSnap) => (docSnap.exists() ? docSnap.data() : undefined)),
+        // after getting the blog post we cache it for offline viewing
+        tap((post) => {
+          if (post) {
+            this.localStorageService.setItem(`blog-post-${id}`, post);
+          }
+        }),
+        catchError(() => {
+          const cachedPost = this.localStorageService.getItem(
+            `blog-post-${id}`
+          );
+          return cachedPost ? of(cachedPost) : of(undefined);
+        })
+      );
+    }
+    // IF USER IS OFFLINE GET BLOG POST FROM LOCAL STORAGE
+    else {
+      const cachedPost = this.localStorageService.getItem(`blog-post-${id}`);
+      return cachedPost ? of(cachedPost) : of(undefined);
+    }
   }
 
   createBlogPost(post: DocumentData) {
@@ -42,15 +83,19 @@ export class BlogPostService {
   }
 
   // Update a blog post by ID
-  // updateBlogPost(id: string, post: DocumentData) {
-  //   const postDoc = doc(this.firestore, 'posts', id);
-  //   return from(getDoc(postDoc)).pipe(
-  //     map((docSnap) => (docSnap.exists() ? docSnap.data() : undefined))
-  //   );
-  // }
-
   updateBlogPost(id: string, post: Partial<DocumentData>) {
     const postDoc = doc(this.firestore, 'posts', id);
-    return from(updateDoc(postDoc, post));
+    return from(updateDoc(postDoc, post)).pipe(
+      tap(() => {
+        // after updating the blog post update the cache to keep things in sync
+        const cachedPost = this.localStorageService.getItem(`blog-post-${id}`);
+        if (cachedPost) {
+          this.localStorageService.setItem(`blog-post-${id}`, {
+            ...cachedPost,
+            ...post,
+          });
+        }
+      })
+    );
   }
 }
